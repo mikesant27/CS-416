e6e7efdef80a5e7eeb8e17a69f4f443e597fbe8b
Michael <mikesant27@gmail.com>
2024-09-26 11:12:08 -0400

    added image to main_city and styling

--- Assignment 2/main.php
+++ Assignment 2/main.php
@@ -1,5 +1,5 @@
 <div class="col-6 col-s-9">
-    <h1>The City</h1>
+    <h1>Welcome!</h1>
     <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts,
     the old town and the modern city.</p>
 </div>
--- Assignment 2/main_city.php
+++ Assignment 2/main_city.php
@@ -2,4 +2,5 @@
     <h1>The City</h1>
     <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts,
     the old town and the modern city.</p>
+    <img src="images/Chania.png" alt="City of Chania">
 </div>
