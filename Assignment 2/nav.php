3950a2ba81e4bc0b1ea3d9775ffb45ebc8023a45
Michael <mikesant27@gmail.com>
2024-09-25 22:09:54 -0400

    removed unneeded files

--- Assignment 2/nav.php
+++ Assignment 2/nav.php
@@ -1,6 +1,6 @@
 <div class="col-3 col-s-3 menu">
             <ul>
-                <li>The Flight</li>
+                <li><a href="flight.php">The Flight</a></li>
                 <li>The City</li>
                 <li>The Island</li>
                 <li>The Food</li>
