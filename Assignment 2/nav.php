77c4c6286e9d6b4199252cf28a0a7f26ce1957ec
Michael <mikesant27@gmail.com>
2024-09-26 14:43:14 -0400

    edited formatting and added missing </ul> tag

--- Assignment 2/nav.php
+++ Assignment 2/nav.php
@@ -1,8 +1,9 @@
 <div class="col-3 col-s-3 menu">
-            <ul>
-                <li><a class="active" href="index.php">Home <i class="fas fa-home"></i> </a></li>
-                <li><a href="flight.php">The Flight <i class="fas fa-plane"></i></a></li>
-                <li><a href="city.php">The City <i class="fas fa-city"></i></a></li>
-                <li><a href="island.php">The Island <i class="fas fa-umbrella-beach"></i></a></li>
-                <li><a href="food.php">The Food <i class="fas fa-utensils"></i></a></li>
-        </div>
+    <ul>
+        <li><a class="active" href="index.php">Home <i class="fas fa-home"></i> </a></li>
+        <li><a href="flight.php">The Flight <i class="fas fa-plane"></i></a></li>
+        <li><a href="city.php">The City <i class="fas fa-city"></i></a></li>
+        <li><a href="island.php">The Island <i class="fas fa-umbrella-beach"></i></a></li>
+        <li><a href="food.php">The Food <i class="fas fa-utensils"></i></a></li>
+    </ul>
+</div>
