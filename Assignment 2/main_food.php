562534e95f0556510ff1e7b8e5afeaecab07a626
Michael <mikesant27@gmail.com>
2024-09-26 13:11:59 -0400

    added images

--- Assignment 2/main_food.php
+++ Assignment 2/main_food.php
@@ -1,4 +1,5 @@
 <div class="col-6 col-s-9">
     <h1>The Food</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, deserunt. Mollitia optio id, esse ducimus repudiandae nesciunt laboriosam tenetur voluptatibus. Odio doloribus iusto quis! Omnis fugit debitis voluptatibus nesciunt asperiores.</p>
+    <img src="images/chania-food.jpg" alt="Chania Food">
 </div>
--- Assignment 2/main_island.php
+++ Assignment 2/main_island.php
@@ -2,4 +2,5 @@
     <h1>The Island</h1>
     <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts,
     the old town and the modern city.</p>
+    <img src="images/chainia-island.jpg" alt="The Island of Chania">
 </div>
