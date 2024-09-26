7297baeb9b7c5b5ddd8b48576dc5f975166f861f
Michael <mikesant27@gmail.com>
2024-09-26 10:22:36 -0400

    added icons to nav buttons

--- Assignment 2/nav.php
+++ Assignment 2/nav.php
@@ -1,9 +1,8 @@
 <div class="col-3 col-s-3 menu">
             <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="flight.php">The Flight</a></li>
-                <li><a href="city.php">The City</a></li>
-                <li><a href="island.php">The Island</a></li>
-                <li><a href="food.php">The Food</a></li>
-            </ul>
+                <li><a class="active" href="index.php">Home <i class="fas fa-home"></i> </a></li>
+                <li><a href="flight.php">The Flight <i class="fas fa-plane"></i></a></li>
+                <li><a href="city.php">The City <i class="fas fa-city"></i></a></li>
+                <li><a href="island.php">The Island <i class="fas fa-umbrella-beach"></i></a></li>
+                <li><a href="food.php">The Food <i class="fas fa-utensils"></i></a></li>
         </div>
