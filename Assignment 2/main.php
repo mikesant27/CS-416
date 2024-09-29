2f93975e549491d443ceb39772e34fcf6899a7ce
Michael <mikesant27@gmail.com>
2024-09-29 14:29:45 -0400

    changed file extension (used png insead of jpeg)

--- Assignment 2/main.php
+++ Assignment 2/main.php
@@ -2,5 +2,5 @@
     <h1>Welcome!</h1>
     <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts,
     the old town and the modern city.</p>
-    <img src="images/mosque.png" alt="Mosque in Chania">
+    <img src="images/mosque.jpeg" alt="Mosque in Chania">
 </div>
