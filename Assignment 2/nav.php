97b67dc8400e8c2d4a9eea7067029d4a40275512
Michael <mikesant27@gmail.com>
2024-09-29 12:59:09 -0400

    added banner to nav, added fonts from fonts lab

--- Assignment 2/nav.php
+++ Assignment 2/nav.php
@@ -1,6 +1,6 @@
 <div class="col-3 col-s-3 menu">
     <ul>
-        <li><a class="active" href="index.php">Home <i class="fas fa-home"></i> </a></li>
+        <li><a class="active" href="index.php">Home <i class="fas fa-home"></i></a></li>
         <li><a href="flight.php">The Flight <i class="fas fa-plane"></i></a></li>
         <li><a href="city.php">The City <i class="fas fa-city"></i></a></li>
         <li><a href="island.php">The Island <i class="fas fa-umbrella-beach"></i></a></li>
