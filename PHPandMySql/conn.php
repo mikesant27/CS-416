2e47172e8854be70945b3e4a9558416d43531cbd
mikesant27 <mikesant27@gmail.com>
2024-10-10 13:59:35 -0400

    added files from blackboard (PHPandMySql)

---
+++ PHPandMySql/conn.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Connected.';
+} catch (PDOException $e) {
+    echo 'error in Connection'  . $e->getMessage();
+}
